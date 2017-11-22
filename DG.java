a196c2334637d5bc8f6527a3ea8ae6f8c4b1027b
Dhananjay Sudhakar Gatade <DS00359937@techmahindra.com>
2017-11-22 14:30:42 +0530

    New project

---
+++ DG.java
@@ -0,0 +1,11 @@
+
+public class DG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my First Project in Java By Dg and Bunty");
+		
+
+	}
+}
